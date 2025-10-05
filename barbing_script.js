2d2a7f89c25f87731c6136eb9d94cb8be69eea36
fortunefelix <felixfortune82@gmail.com>
2025-10-05 04:02:27 +0100

    first commit

---
+++ barbing_script.js
@@ -0,0 +1,377 @@
+let get_Body_Overlay = document.getElementById("body_overlay");
+let get_First_Image =  document.querySelector(".first_big_image");
+let get_Second_Image =  document.querySelector(".second_big_image");
+let get_Third_Image =  document.querySelector(".third_big_image");
+let get_Fourth_Image =  document.querySelector(".fourth_big_image");
+let get_Fifth_Image =  document.querySelector(".fifth_big_image");
+let get_Sixth_Image =  document.querySelector(".sixth_big_image");
+let get_Seventh_Image =  document.querySelector(".seventh_big_image");
+let get_Eight_Image =  document.querySelector(".eight_big_image");
+let get_Ninth_Image =  document.querySelector(".ninth_big_image");
+let get_Tenth_Image =  document.querySelector(".tenth_big_image");
+let get_Eleventh_Image =  document.querySelector(".eleventh_big_image");
+let get_Twelfth_Image =  document.querySelector(".twelfth_big_image");
+let get_Thirteenth_Image =  document.querySelector(".thirteenth_big_image");
+let get_Fourteenth_Image =  document.querySelector(".fourteenth_big_image");
+let get_Fifteenth_Image =  document.querySelector(".fifteenth_big_image");
+let get_Body = document.querySelector("body");
+let get_First_Video = document.getElementById("first_video");
+let get_Second_Video = document.getElementById("second_video");
+let get_Third_Video = document.getElementById("third_video");
+let get_Fourth_Video = document.getElementById("fourth_video");
+let get_Fifth_Video = document.getElementById("fifth_video");
+let get_Sixth_Video = document.getElementById("sixth_video");
+let get_Seventh_Video = document.getElementById("seventh_video");
+
+function enlarge_First_Image(){
+    get_Body_Overlay.style.display = "block";
+	get_First_Image.style.visibility = "visible";
+	get_Fifth_Image.style.visibility = "hidden";	
+	get_Second_Image.style.visibility = "hidden";
+	get_Third_Image.style.visibility = "hidden";
+	get_Fourth_Image.style.visibility = "hidden";	
+	get_Seventh_Image.style.visibility = "hidden";
+	get_Eight_Image.style.visibility = "hidden";
+	get_Ninth_Image.style.visibility = "hidden";
+	get_Tenth_Image.style.visibility = "hidden";
+	get_Sixth_Image.style.visibility = "hidden";
+	get_Eleventh_Image.style.visibility = "hidden";
+	get_Twelfth_Image.style.visibility = "hidden";
+	get_Thirteenth_Image.style.visibility = "hidden";
+	get_Fourteenth_Image.style.visibility = "hidden";
+	get_Fifteenth_Image.style.visibility = "hidden";	
+	get_Body.style.overflowY = "hidden";
+}
+
+
+function enlarge_Second_Image(){
+    get_Body_Overlay.style.display = "block";
+	get_Second_Image.style.visibility = "visible";
+	get_Fifth_Image.style.visibility = "hidden";	
+	get_First_Image.style.visibility = "hidden";
+	get_Third_Image.style.visibility = "hidden";
+	get_Fourth_Image.style.visibility = "hidden";	
+	get_Seventh_Image.style.visibility = "hidden";
+	get_Eight_Image.style.visibility = "hidden";
+	get_Ninth_Image.style.visibility = "hidden";
+	get_Tenth_Image.style.visibility = "hidden";
+	get_Sixth_Image.style.visibility = "hidden";
+	get_Eleventh_Image.style.visibility = "hidden";
+	get_Twelfth_Image.style.visibility = "hidden";
+	get_Thirteenth_Image.style.visibility = "hidden";
+	get_Fourteenth_Image.style.visibility = "hidden";
+	get_Fifteenth_Image.style.visibility = "hidden";	
+	get_Body.style.overflowY = "hidden";
+}
+
+
+function enlarge_Third_Image(){
+    get_Body_Overlay.style.display = "block";
+	get_Third_Image.style.visibility = "visible";
+	get_Fifth_Image.style.visibility = "hidden";	
+	get_Second_Image.style.visibility = "hidden";
+	get_First_Image.style.visibility = "hidden";
+	get_Fourth_Image.style.visibility = "hidden";
+	get_Seventh_Image.style.visibility = "hidden";
+	get_Eight_Image.style.visibility = "hidden";
+	get_Ninth_Image.style.visibility = "hidden";
+	get_Tenth_Image.style.visibility = "hidden";
+	get_Twelfth_Image.style.visibility = "hidden";	
+	get_Eleventh_Image.style.visibility = "hidden";
+	get_Thirteenth_Image.style.visibility = "hidden";
+	get_Fourteenth_Image.style.visibility = "hidden";
+	get_Fifteenth_Image.style.visibility = "hidden";	
+	get_Body.style.overflowY = "hidden";
+}
+function enlarge_Fourth_Image(){
+    get_Body_Overlay.style.display = "block";
+	get_Fourth_Image.style.visibility = "visible";
+	get_Fifth_Image.style.visibility = "hidden";	
+	get_Third_Image.style.visibility = "hidden";
+	get_Second_Image.style.visibility = "hidden";
+	get_First_Image.style.visibility = "hidden";
+	get_Seventh_Image.style.visibility = "hidden";
+	get_Eight_Image.style.visibility = "hidden";
+	get_Ninth_Image.style.visibility = "hidden";
+	get_Tenth_Image.style.visibility = "hidden";
+	get_Sixth_Image.style.visibility = "hidden";
+	get_Twelfth_Image.style.visibility = "hidden";	
+	get_Eleventh_Image.style.visibility = "hidden";
+	get_Thirteenth_Image.style.visibility = "hidden";
+	get_Fourteenth_Image.style.visibility = "hidden";
+	get_Fifteenth_Image.style.visibility = "hidden";	
+	get_Body.style.overflowY = "hidden";
+	get_First_Video.play();
+}
+
+
+
+function enlarge_Fifth_Image(){
+    get_Body_Overlay.style.display = "block";
+	get_Fifth_Image.style.visibility = "visible";
+	get_Fourth_Image.style.visibility = "hidden";
+	get_Third_Image.style.visibility = "hidden";
+	get_Second_Image.style.visibility = "hidden";
+	get_First_Image.style.visibility = "hidden";
+	get_Seventh_Image.style.visibility = "hidden";
+	get_Eight_Image.style.visibility = "hidden";
+	get_Ninth_Image.style.visibility = "hidden";
+	get_Tenth_Image.style.visibility = "hidden";
+	get_Sixth_Image.style.visibility = "hidden";
+	get_Twelfth_Image.style.visibility = "hidden";	
+	get_Eleventh_Image.style.visibility = "hidden";
+	get_Thirteenth_Image.style.visibility = "hidden";
+	get_Fifteenth_Image.style.visibility = "hidden";	
+	get_Body.style.overflowY = "hidden";
+}
+function enlarge_Sixth_Image(){
+    get_Body_Overlay.style.display = "block";
+	get_Sixth_Image.style.visibility = "visible";
+	get_Fifth_Image.style.visibility = "hidden";
+	get_Fourth_Image.style.visibility = "hidden";
+	get_Third_Image.style.visibility = "hidden";
+	get_Second_Image.style.visibility = "hidden";
+	get_First_Image.style.visibility = "hidden";
+	get_Seventh_Image.style.visibility = "hidden";
+	get_Eight_Image.style.visibility = "hidden";
+	get_Ninth_Image.style.visibility = "hidden";
+	get_Tenth_Image.style.visibility = "hidden";
+	get_Eleventh_Image.style.visibility = "hidden";	
+	get_Twelfth_Image.style.visibility = "hidden";
+	get_Thirteenth_Image.style.visibility = "hidden";
+	get_Fourteenth_Image.style.visibility = "hidden";
+	get_Fifteenth_Image.style.visibility = "hidden";	
+	get_Body.style.overflowY = "hidden";
+}
+
+
+function enlarge_Seventh_Image(){
+    get_Body_Overlay.style.display = "block";
+	get_Seventh_Image.style.visibility = "visible";
+	get_Fifth_Image.style.visibility = "hidden";	
+	get_Fourth_Image.style.visibility = "hidden";
+	get_Third_Image.style.visibility = "hidden";
+	get_Second_Image.style.visibility = "hidden";
+	get_First_Image.style.visibility = "hidden";
+	get_Eight_Image.style.visibility = "hidden";
+	get_Ninth_Image.style.visibility = "hidden";	
+	get_Tenth_Image.style.visibility = "hidden";
+	get_Eleventh_Image.style.visibility = "hidden";	
+	get_Sixth_Image.style.visibility = "hidden";
+	get_Twelfth_Image.style.visibility = "hidden";
+	get_Thirteenth_Image.style.visibility = "hidden";
+	get_Fourteenth_Image.style.visibility = "hidden";
+	get_Fifteenth_Image.style.visibility = "hidden";	
+	get_Body.style.overflowY = "hidden";
+	get_Second_Video.play();
+}
+
+
+
+
+function enlarge_Eight_Image(){
+    get_Body_Overlay.style.display = "block";
+	get_Eight_Image.style.visibility = "visible";
+	get_Fifth_Image.style.visibility = "hidden";	
+	get_Ninth_Image.style.visibility = "hidden";	
+	get_Seventh_Image.style.visibility = "hidden";
+	get_Fourth_Image.style.visibility = "hidden";
+	get_Third_Image.style.visibility = "hidden";
+	get_Second_Image.style.visibility = "hidden";
+	get_First_Image.style.visibility = "hidden";
+	get_Sixth_Image.style.visibility = "hidden";	
+	get_Eleventh_Image.style.visibility = "hidden";
+	get_Twelfth_Image.style.visibility = "hidden";
+	get_Thirteenth_Image.style.visibility = "hidden";
+	get_Fourteenth_Image.style.visibility = "hidden";
+	get_Fifteenth_Image.style.visibility = "hidden";	
+	get_Body.style.overflowY = "hidden";
+	get_Third_Video.play();
+}
+
+
+
+
+function enlarge_Ninth_Image(){
+    get_Body_Overlay.style.display = "block";
+	get_Ninth_Image.style.visibility = "visible";
+	get_Fifth_Image.style.visibility = "hidden";	
+	get_Tenth_Image.style.visibility = "hidden";	
+	get_Eight_Image.style.visibility = "hidden";
+	get_Seventh_Image.style.visibility = "hidden";
+	get_Fourth_Image.style.visibility = "hidden";
+	get_Third_Image.style.visibility = "hidden";
+	get_Second_Image.style.visibility = "hidden";
+	get_First_Image.style.visibility = "hidden";
+	get_Eleventh_Image.style.visibility = "hidden";	
+	get_Sixth_Image.style.visibility = "hidden";
+	get_Twelfth_Image.style.visibility = "hidden";
+	get_Thirteenth_Image.style.visibility = "hidden";
+	get_Fourteenth_Image.style.visibility = "hidden";
+	get_Fifteenth_Image.style.visibility = "hidden";	
+	get_Body.style.overflowY = "hidden";
+	get_Fourth_Video.play();
+}
+
+
+
+function enlarge_Tenth_Image(){
+    get_Body_Overlay.style.display = "block";
+	get_Tenth_Image.style.visibility = "visible";
+	get_Fifth_Image.style.visibility = "hidden";	
+	get_Ninth_Image.style.visibility = "hidden";
+	get_Eight_Image.style.visibility = "hidden";
+	get_Seventh_Image.style.visibility = "hidden";
+	get_Fourth_Image.style.visibility = "hidden";
+	get_Third_Image.style.visibility = "hidden";
+	get_Second_Image.style.visibility = "hidden";
+	get_First_Image.style.visibility = "hidden";
+	get_Eleventh_Image.style.visibility = "hidden";
+	get_Sixth_Image.style.visibility = "hidden";
+	get_Twelfth_Image.style.visibility = "hidden";
+	get_Thirteenth_Image.style.visibility = "hidden";
+	get_Fourteenth_Image.style.visibility = "hidden";
+	get_Fifteenth_Image.style.visibility = "hidden";	
+	get_Body.style.overflowY = "hidden";
+	get_Fifth_Video.play();
+}
+
+
+function enlarge_Eleventh_Image(){
+    get_Body_Overlay.style.display = "block";
+	get_Eleventh_Image.style.visibility = "visible";
+	get_Sixth_Image.style.visibility = "hidden";
+	get_Fifth_Image.style.visibility = "visible";
+	get_Fourth_Image.style.visibility = "hidden";
+	get_Third_Image.style.visibility = "hidden";
+	get_Second_Image.style.visibility = "hidden";
+	get_First_Image.style.visibility = "hidden";
+	get_Seventh_Image.style.visibility = "hidden";
+	get_Eight_Image.style.visibility = "hidden";
+	get_Ninth_Image.style.visibility = "hidden";
+	get_Tenth_Image.style.visibility = "hidden";
+	get_Twelfth_Image.style.visibility = "hidden";
+	get_Thirteenth_Image.style.visibility = "hidden";
+	get_Fourteenth_Image.style.visibility = "hidden";
+	get_Fifteenth_Image.style.visibility = "hidden";	
+	get_Body.style.overflowY = "hidden";
+}
+function enlarge_Twelfth_Image(){
+    get_Body_Overlay.style.display = "block";
+	get_Twelfth_Image.style.visibility = "visible";
+	get_Eleventh_Image.style.visibility = "hidden";
+	get_Sixth_Image.style.visibility = "hidden";
+	get_Fifth_Image.style.visibility = "visible";
+	get_Fourth_Image.style.visibility = "hidden";
+	get_Third_Image.style.visibility = "hidden";
+	get_Second_Image.style.visibility = "hidden";
+	get_First_Image.style.visibility = "hidden";
+	get_Seventh_Image.style.visibility = "hidden";
+	get_Eight_Image.style.visibility = "hidden";
+	get_Ninth_Image.style.visibility = "hidden";
+	get_Tenth_Image.style.visibility = "hidden";
+	get_Thirteenth_Image.style.visibility = "hidden";
+	get_Fourteenth_Image.style.visibility = "hidden";
+	get_Fifteenth_Image.style.visibility = "hidden";	
+	get_Body.style.overflowY = "hidden";
+}
+
+
+function enlarge_Thirteenth_Image(){
+    get_Body_Overlay.style.display = "block";
+	get_Thirteenth_Image.style.visibility = "visible";
+	get_Twelfth_Image.style.visibility = "hidden";
+	get_Eleventh_Image.style.visibility = "hidden";
+	get_Sixth_Image.style.visibility = "hidden";
+	get_Fifth_Image.style.visibility = "hidden";
+	get_Fourth_Image.style.visibility = "hidden";
+	get_Third_Image.style.visibility = "hidden";
+	get_Second_Image.style.visibility = "hidden";
+	get_First_Image.style.visibility = "hidden";
+	get_Seventh_Image.style.visibility = "hidden";
+	get_Eight_Image.style.visibility = "hidden";
+	get_Ninth_Image.style.visibility = "hidden";
+	get_Tenth_Image.style.visibility = "hidden";
+	get_Fourteenth_Image.style.visibility = "hidden";
+	get_Fifteenth_Image.style.visibility = "hidden";	
+	get_Body.style.overflowY = "hidden";
+	get_Sixth_Video.play();
+}
+
+
+function enlarge_Fourteenth_Image(){
+    get_Body_Overlay.style.display = "block";
+	get_Fourteenth_Image.style.visibility = "visible";
+	get_Fifteenth_Image.style.visibility = "hidden";	
+	get_Thirteenth_Image.style.visibility = "hidden";
+	get_Twelfth_Image.style.visibility = "hidden";
+	get_Eleventh_Image.style.visibility = "hidden";
+	get_Sixth_Image.style.visibility = "hidden";
+	get_Fifth_Image.style.visibility = "hidden";
+	get_Fourth_Image.style.visibility = "hidden";
+	get_Third_Image.style.visibility = "hidden";
+	get_Second_Image.style.visibility = "hidden";
+	get_First_Image.style.visibility = "hidden";
+	get_Seventh_Image.style.visibility = "hidden";
+	get_Eight_Image.style.visibility = "hidden";
+	get_Ninth_Image.style.visibility = "hidden";
+	get_Tenth_Image.style.visibility = "hidden";	
+	get_Body.style.overflowY = "hidden";
+	get_Seventh_Video.play();
+}
+
+function enlarge_Fifteenth_Image(){
+    get_Body_Overlay.style.display = "block";
+	get_Fifteenth_Image.style.visibility = "visible";
+	get_Fourteenth_Image.style.visibility = "hidden";
+	get_Thirteenth_Image.style.visibility = "hidden";
+	get_Twelfth_Image.style.visibility = "hidden";
+	get_Eleventh_Image.style.visibility = "hidden";
+	get_Sixth_Image.style.visibility = "hidden";
+	get_Fifth_Image.style.visibility = "hidden";
+	get_Fourth_Image.style.visibility = "hidden";
+	get_Third_Image.style.visibility = "hidden";
+	get_Second_Image.style.visibility = "hidden";
+	get_First_Image.style.visibility = "hidden";
+	get_Seventh_Image.style.visibility = "hidden";
+	get_Eight_Image.style.visibility = "hidden";
+	get_Ninth_Image.style.visibility = "hidden";
+	get_Tenth_Image.style.visibility = "hidden";	
+	get_Body.style.overflowY = "hidden";
+}
+
+function dismiss_Image(){
+    get_Body_Overlay.style.display = "none";
+	get_Body.style.overflowY = "visible";
+	get_First_Video.pause();
+	get_Second_Video.pause();
+    get_Third_Video.pause();	
+	get_Fourth_Video.pause();
+	get_Fifth_Video.pause();
+	get_Sixth_Video.pause();
+	get_Seventh_Video.pause();
+}
+
+
+let slide_Index = 1;
+show_Slide(slide_Index);
+
+function plus_Slides(n){
+	show_Slide(slide_Index += n);
+}
+
+
+function show_Slide(n){
+	let get_Image_Container = document.getElementsByClassName("barbing_images_slide");
+	let i;
+	if(n > get_Image_Container.length){
+		slide_Index = 1;
+	}
+	if(n < 1){
+		slide_Index = get_Image_Container.length;
+	}	
+	for(i = 0; i < get_Image_Container.length; i++){
+		get_Image_Container[i].style.display = "none";
+	}
+	get_Image_Container[slide_Index - 1].style.display = "inline";
+}
